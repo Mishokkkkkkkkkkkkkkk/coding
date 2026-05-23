0579016eef6357211c3fcd9c021f18788c6f4d03
Mishokkkkkkkkkkkkkkk <2012.misha.nam@gmail.com>
2026-05-23 20:29:35 +0900

    BIBA I BOB
    BOB STROITEL

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //etokaroche gavarit hello world
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("hello_world");
+    }
+}
